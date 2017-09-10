7deb97707f08558f3fefdacf19c910f1c14d14e0
Ken Matsuoka <matsken@jp.ibm.com>
2017-09-10 21:26:42 +0900

    updates

--- src/providers/StashTreeProvider.ts
+++ src/providers/StashTreeProvider.ts
@@ -1,11 +1,28 @@
 import * as vscode from "vscode";
 import * as cp from "child_process";
 
-export class StashTreeProvider implements vscode.TreeDataProvider<Stash> {
+export class StashTreeProvider implements vscode.TreeDataProvider<vscode.TreeItem> {
 	constructor(private workspaceRoot: string) {
 	}
 
-	getChildren(stash?: Stash): Thenable<Stash[]> {
+	getChildren(stash?: Stash): Thenable<vscode.TreeItem[]> {
+		if (stash) {
+			if (stash instanceof Stash) {
+				return new Promise<vscode.TreeItem[]>((resolve, reject) => {
+					cp.exec("git stash show " + stash.id, {
+						cwd: this.workspaceRoot
+					}, (err, stdout) => {
+						if (err) {
+							console.log(err);
+							return reject("error running git stash show");
+						}
+						return resolve(stdout.trim().split("\n").map((line) => new vscode.TreeItem(line)));
+					});
+				});
+			} else {
+				return Promise.resolve([]);
+			}
+		}
 		return new Promise<Stash[]>((resolve, reject) => {
 			cp.exec("git stash list", {
 				cwd: this.workspaceRoot
@@ -14,7 +31,14 @@ export class StashTreeProvider implements vscode.TreeDataProvider<Stash> {
 					console.log(err);
 					return reject('git not found');
 				}
-				return resolve([this.createItem(stdout)]);
+				const arr = stdout.trim().split("\n");
+				return resolve(arr.map((item) => {
+					const comps = item.split(":");
+					const id = comps[0].split("{")[1].split("}")[0];
+					const branch = comps[1].trim().split("WIP on ")[0];
+					const commit = comps[2].trim().split(" ")[0];
+					return this.createItem(branch, commit, id, item);
+				}));
 			});
 		});
 	}
@@ -23,19 +47,22 @@ export class StashTreeProvider implements vscode.TreeDataProvider<Stash> {
 		return stash;
 	}
 
-	createItem(label) {
-		return new Stash(label, vscode.TreeItemCollapsibleState.Collapsed);
+	createItem(branch, commit, id, label) {
+		return new Stash(branch, commit, id, label, vscode.TreeItemCollapsibleState.Collapsed);
 	}
 }
 
 class Stash extends vscode.TreeItem {
-	// constructor(
-	// 	public readonly label: string,
-	// 	public readonly collapsibleState: vscode.TreeItemCollapsibleState,
-	// 	public readonly command?: vscode.Command
-	// ) {
-	// 	super(label, collapsibleState);
-	// }
+	constructor(
+		public readonly branch: string,
+		public readonly commit: string,
+		public readonly id: number,
+		public readonly label: string,
+		public readonly collapsibleState: vscode.TreeItemCollapsibleState,
+		public readonly command?: vscode.Command,
+	) {
+		super(label, collapsibleState);
+	}
 
 	// iconPath = {
 	// 	light: path.join(__filename, '..', '..', '..', 'resources', 'light', 'dependency.svg'),
